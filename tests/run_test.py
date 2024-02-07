de1a6afb755a7795e0f0db196c485a96367c72aa
Lev Kozlov <kozlov.l.a10@gmail.com>
2024-02-07 15:24:02 +0300

    feat: add collision handler bindings

--- tests/run_test.py
+++ tests/run_test.py
@@ -1,10 +1,18 @@
 import casadi_kin_dyn.casadi_kin_dyn as cas_kin_dyn
+from robot_descriptions.a1_description import URDF_PATH
+import numpy as np
 
-urdf = open("cart_pole.urdf", "r").read()
-kindyn = cas_kin_dyn.CasadiKinDyn(urdf)
+urdf = open(URDF_PATH, "r").read()
+kindyn = cas_kin_dyn.CasadiKinDyn(
+    urdf, root_joint=cas_kin_dyn.CasadiKinDyn.JointType.FREE_FLYER
+)
 
 print(kindyn.nq())
 print(kindyn.potentialEnergy())
-print(kindyn.jacobian("mass", cas_kin_dyn.CasadiKinDyn.LOCAL))
-print(kindyn.jacobianTimeVariation("mass", cas_kin_dyn.CasadiKinDyn.LOCAL))
 print(kindyn.jacobianCenterOfMass(True))
+
+c = cas_kin_dyn.CollisionHandler(kindyn)
+
+q = np.random.randn(kindyn.nq())
+d = np.zeros((c.numPairs(),))
+J = np.zeros((c.numPairs(), kindyn.nv()), order="F")
